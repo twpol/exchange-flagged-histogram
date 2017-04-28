dc15e33f623236774a32dff44d8b89eb02a0e610
James Ross <git@james-ross.co.uk>
2017-04-28 19:41:38 +0100

    Initial commit of .NET 4.6 app

---
+++ Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace exchange_flagged_histogram
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
